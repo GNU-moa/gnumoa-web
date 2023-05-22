8f0335366621c4a29fef209c5e919c1ed134a758
Jang-DongHo <donghoo18@naver.com>
2023-05-22 20:01:27 +0900

    [Refactor] 코드 구조 변경

--- public/js/data/firebase.js
+++ public/js/data/firebase.js
@@ -3,8 +3,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js";
 import { getFunctions } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-functions.js";
 
-const response = await fetch("/gnu-moa-firebase-adminsdk.json");
-const firebaseConfig = await response.json();
+const firebaseConfig = {
+  apiKey: "AIzaSyD2IgfUYPhtaMPDf9KzylxIS8sma55nzak",
+  authDomain: "gnu-moa.firebaseapp.com",
+  projectId: "gnu-moa",
+  storageBucket: "gnu-moa.appspot.com",
+  messagingSenderId: "847390840165",
+  appId: "1:847390840165:web:034e0524a15ff012807d8c",
+  measurementId: "G-DVMHL25R2P",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
