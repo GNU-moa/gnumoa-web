f86818049b7cc8fc865e904ef9c3b63eea297105
Jang-DongHo <donghoo18@naver.com>
2023-06-01 13:48:36 +0900

    [Fix] 경영대학 학과 아이콘 수정

--- public/js/departmentMenu.js
+++ public/js/departmentMenu.js
@@ -1,50 +1,29 @@
-// // 학과 토글
-// const departments = document.querySelectorAll(".department");
+// 학과 토글
+const departments = document.querySelectorAll(".department");
 
-// departments.forEach((department) => {
-//   const departmentHeader = department.querySelector(".department_header");
-//   const college = department.querySelector(".department_content").innerHTML;
-//   const toggleIcon = department.querySelector(".toggle_icon");
-//   const departmentDetail = department.querySelector(".department_detail");
-//   const departmentNames = departmentDetail.getElementsByTagName("a");
-//   for (let i = 0; i < departmentNames.length; i++) {
-//     const departmentName = departmentNames[i].innerHTML;
-//     departmentNames[i].href = `./department.html?college=${college}&department=${departmentName}`;
-//   }
+departments.forEach((department) => {
+  const departmentHeader = department.querySelector(".department_header");
+  const college = department.querySelector(".department_content").innerHTML;
+  const toggleIcon = department.querySelector(".toggle_icon");
+  const departmentDetail = department.querySelector(".department_detail");
+  const departmentNames = departmentDetail.getElementsByTagName("a");
+  for (let i = 0; i < departmentNames.length; i++) {
+    const departmentName = departmentNames[i].innerHTML;
+    departmentNames[
+      i
+    ].href = `./department.html?college=${college}&department=${departmentName}`;
+  }
 
-//   let isExpanded = false; // 초기 상태 설정
-
-//   departmentHeader.addEventListener("click", () => {
-//     if (!isExpanded) {
-//       departmentDetail.style.display = "block";
-//       toggleIcon.innerHTML = '<i class="fas fa-caret-up"></i>';
-//       isExpanded = true;
-//     } else {
-//       departmentDetail.style.display = "none";
-//       toggleIcon.innerHTML = '<i class="fas fa-caret-down"></i>';
-//       isExpanded = false;
-//     }
-//   });
-// });
-
-const departmentContents = document.querySelectorAll('.department_content');
-
-departmentContents.forEach((departmentContent) => {
-  const toggleIcon = departmentContent.nextElementSibling;
-  const department = departmentContent.parentElement;
-  const departmentDetail = department.querySelector('.department_detail');
   let isExpanded = false; // 초기 상태 설정
 
-  toggleIcon.addEventListener('click', () => {
+  departmentHeader.addEventListener("click", () => {
     if (!isExpanded) {
-      departmentDetail.style.display = 'block';
+      departmentDetail.style.display = "block";
       toggleIcon.innerHTML = '<i class="fas fa-caret-up"></i>';
-      department.style.height = 'auto';
       isExpanded = true;
     } else {
-      departmentDetail.style.display = 'none';
+      departmentDetail.style.display = "none";
       toggleIcon.innerHTML = '<i class="fas fa-caret-down"></i>';
-      department.style.height = '50px';
       isExpanded = false;
     }
   });
